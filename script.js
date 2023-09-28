15a5f56e8bf914f8214fbb8ef25d65570163027b
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-28 15:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	var firstName = document.forms["form1"]["fname"].value;
+  var lastName = document.forms["form1"]["lname"].value;
+	
+  if (firstName !== "" && lastName !== "") {
+    // Display the first and last name using alert
+    alert(firstName + " " + lastName);
+  } 
+  // Prevent the form from submitting and refreshing the page
+  return false;
 
 }
